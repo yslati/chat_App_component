2ce62f32a6e96fa813481f5ec27b7c833cbaf7fb
yslati <yslati@student.1337.ma>
2022-03-07 15:32:32 +0100

    create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center bg-red-300">
+      <div className='w-80 h-96 text-red-400 bg-slate-300'>
+        hello
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
